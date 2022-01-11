3dc4dfe7641e1fa4d536b1273730a1b42d154605
alshohatee <alshohateeaseel@gmail.com>
2022-01-10 21:41:48 -0500

    Select all of the list items with a class of second:

--- index.js
+++ index.js
@@ -11,14 +11,18 @@ let sectionElUsingId = document.getElementById('container')
 
 // 2) Select the section with an id of container using querySelector.
 let sectionElUsingQuery = document.querySelector('section')
-    // sectionElUsingQuery.style.display = "none";
-    // 3) Select all of the list items with a class of "second".
-    // 4) Select a list item with a class of third, but only the list item inside of the ol tag.
-    // 5) Give the section with an id of container the text "Hello!".
-    // 6) Add the class main to the div with a class of footer.
-    // 7) Remove the class main on the div with a class of footer.
-    // 8) Create a new li element.
-    // 9) Give the li the text "four".
-    // 10) Append the li to the ul element.
-    // 12) Loop over all of the lis inside the ol tag and give them a background color of "green".
-    // 13) Remove the div with a class of footer.
+
+// 3) Select all of the list items with a class of "second".
+// This will return list of all the elements with class name of second
+let listItemsSecond = document.getElementsByClassName('second')
+
+// 4) Select a list item with a class of third, but only the list item inside of the ol tag.
+listItemsSecond.style.display = "none";
+// 5) Give the section with an id of container the text "Hello!".
+// 6) Add the class main to the div with a class of footer.
+// 7) Remove the class main on the div with a class of footer.
+// 8) Create a new li element.
+// 9) Give the li the text "four".
+// 10) Append the li to the ul element.
+// 12) Loop over all of the lis inside the ol tag and give them a background color of "green".
+// 13) Remove the div with a class of footer.
